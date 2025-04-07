d28f00b931b05740798d7f80598dbfb5c7516968
Lucas Lebugle <lu.lebugl@student.42.fr>
2025-04-08 01:35:23 +0200

    starting ex01

---
+++ cpp-00/ex01/PhoneBook.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llebugle <lucas.lebugle@student.s19.be>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 01:22:27 by llebugle          #+#    #+#             */
+/*   Updated: 2025/04/08 01:35:08 by llebugle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class Contact {
+	std::string name;
+};
+
+class PhoneBook{
+	Contact contact[8];
+};
+
+int	main(void)
+{
+	std::string input{};
+	return (0);
+}

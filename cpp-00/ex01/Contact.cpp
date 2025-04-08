e0e8c24ec892b2463a1cebb5ee69c437c45bde45
Lucas Lebugle <lu.lebugl@student.42.fr>
2025-04-08 22:54:27 +0200

    added contact classes and methods

--- cpp-00/ex01/Contact.cpp
+++ cpp-00/ex01/Contact.cpp
@@ -6,11 +6,13 @@
 /*   By: llebugle <lucas.lebugle@student.s19.be>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 21:56:24 by llebugle          #+#    #+#             */
-/*   Updated: 2025/04/08 22:16:52 by llebugle         ###   ########.fr       */
+/*   Updated: 2025/04/08 22:52:24 by llebugle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
+
+#include <iostream>
 
 Contact::Contact() {
     _firstname = "";
@@ -29,3 +31,77 @@ Contact::Contact(const std::string& firstname, const std::string& lastname,
     _number = phone_number;
     _secret = secret;
 }
+
+bool Contact::_isValidName(const std::string& name) const {
+    if (name.empty()) {
+        std::cout << "Error: Field cannot be empty.\n";
+        return false;
+    }
+    for (size_t i = 0; i < name.length(); i++) {
+        if (!isalpha(name[i]) && name[i] != ' ' && name[i] != '-') {
+            std::cout << "Error: Names can only contain letters, spaces and "
+                         "hyphens.\n";
+            return false;
+        }
+    }
+}
+
+bool Contact::_isValidPhoneNumber(const std::string& number) const {
+    if (number.empty()) {
+        std::cout << "Error: Field cannot be empty.\n";
+        return false;
+    }
+    for (size_t i = 0; i < number.length(); i++) {
+        if (!isnumber(number[i]) && number[i] != ' ') {
+            std::cout
+                << "Error: Phone number can only contain spaces or digits.\n";
+            return false;
+        }
+    }
+}
+
+// Setters
+bool Contact::setFirstName(const std::string& firstname) {
+    if (!_isValidName(firstname)) return false;
+    _firstname = firstname;
+    return true;
+}
+
+bool Contact::setLastName(const std::string& lastname) {
+    if (!_isValidName(lastname)) return false;
+
+    _lastname = lastname;
+    return true;
+}
+
+bool Contact::setNickname(const std::string& nickname) {
+    if (!_isValidName(nickname)) return false;
+    _nickname = nickname;
+    return true;
+}
+
+bool Contact::setPhoneNumber(const std::string& number) {
+    if (!_isValidPhoneNumber(number)) return false;
+    _number = number;
+    return true;
+}
+
+bool Contact::setSecret(const std::string& secret) {
+    if (secret.empty()) {
+        std::cout << "Error: Field cannot be empty.\n";
+        return false;
+    }
+    _secret = secret;
+    return true;
+}
+
+// Getters
+std::string Contact::getFirstName() const { return _firstname; }
+
+std::string Contact::getLastName() const { return _lastname; }
+
+std::string Contact::getNickname() const { return _nickname; }
+
+std::string Contact::getPhoneNumber() const { return _number; }
+
+std::string Contact::getSecret() const { return _secret; }
--- cpp-00/ex01/Contact.hpp
+++ cpp-00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: llebugle <lucas.lebugle@student.s19.be>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 02:38:53 by llebugle          #+#    #+#             */
-/*   Updated: 2025/04/08 22:17:04 by llebugle         ###   ########.fr       */
+/*   Updated: 2025/04/08 22:43:28 by llebugle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,20 @@ class Contact {
     std::string _number;
     std::string _secret;
 
+    bool _isValidName(const std::string& name) const;
+    bool _isValidPhoneNumber(const std::string& name) const;
+
    public:
     Contact();
     Contact(const std::string& firstname, const std::string& lastname,
             const std::string& nickname, const std::string& number,
             const std::string& secret);
     // Setter methods
-    void setFirstName(const std::string& firstname);
-    void setLastName(const std::string& fullname);
-    void setNickname(const std::string& nickname);
-    void setPhoneNumber(const std::string& number);
-    void setSecret(const std::string& secret);
+    bool setFirstName(const std::string& firstname);
+    bool setLastName(const std::string& fullname);
+    bool setNickname(const std::string& nickname);
+    bool setPhoneNumber(const std::string& number);
+    bool setSecret(const std::string& secret);
 
     // Getter methods
     std::string getFirstName() const;

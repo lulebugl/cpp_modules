a9dfe060060e45c95b72fb1efab13841e1652fa7
Lucas Lebugle <lu.lebugl@student.42.fr>
2025-04-08 02:45:01 +0200

    all

--- cpp-00/ex01/Contact.cpp
+++ cpp-00/ex01/Contact.cpp
@@ -1,27 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llebugle <lucas.lebugle@student.s19.be>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/08 01:22:27 by llebugle          #+#    #+#             */
-/*   Updated: 2025/04/08 01:35:08 by llebugle         ###   ########.fr       */
+/*   Created: 2025/04/08 02:38:53 by llebugle          #+#    #+#             */
+/*   Updated: 2025/04/08 02:44:50 by llebugle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 class Contact {
-	std::string name;
+   private:
+    std::string first_name{};
+    std::string last_name{};
+    std::string nickname{};
+    int         phone_number{};
 };
-
-class PhoneBook{
-	Contact contact[8];
-};
-
-int	main(void)
-{
-	std::string input{};
-	return (0);
-}
---
+++ cpp-00/ex01/main.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llebugle <lucas.lebugle@student.s19.be>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 01:22:27 by llebugle          #+#    #+#             */
+/*   Updated: 2025/04/08 02:38:35 by llebugle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class Contact {
+   public:
+    std::string first_name{};
+    std::string last_name{};
+    std::string nickname{};
+    int         phone_number{};
+	Contact		Add();
+};
+
+class PhoneBook {
+    Contact contact[7]{};
+};
+
+enum Command { CMD_ADD, CMD_SEARCH, CMD_EXIT, CMD_UNKNOWN };
+
+Command parseCommand(const std::string& input) {
+    if (input == "ADD") return CMD_ADD;
+    if (input == "SEARCH") return CMD_SEARCH;
+    if (input == "EXIT") return CMD_EXIT;
+    return CMD_UNKNOWN;
+}
+
+Contact Add() {
+    Contact contact;
+    std::cout << "First name: ";
+    std::cin >> contact.first_name;
+    std::cout << "Last name: ";
+	std::cin >> contact.last_name;
+    std::cout << "Nickname: ";
+	std::cin >> contact.nickname;
+    std::cout << "Phone number: ";
+	// protect phone number
+	std::cin >> contact.phone_number;
+	return contact;
+}
+
+int main(void) {
+    PhoneBook phoneBook;
+    while (true) {
+        std::string input;
+        std::cout << "Enter command (ADD, SEARCH, EXIT): ";
+        std::cin >> input;
+        Command cmd = parseCommand(input);
+		int idx = 0;
+
+        switch (cmd) {
+            case CMD_ADD:
+                // Handle add
+                break;
+            case CMD_SEARCH:
+                // Handle search
+                break;
+            case CMD_EXIT:
+                return 0;
+            case CMD_UNKNOWN:
+                std::cout << "Unknown command" << std::endl;
+                break;
+        }
+		if (++idx > 7) idx = 0;
+    }
+    return (0);
+}

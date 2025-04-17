034a16cdc54c14ab32c4580c2f14654ab7eb8a96
llebugle <llebugle@student.s19.be>
2025-04-17 20:59:05 +0200

    fix trailing whitespaces in names

--- cpp-00/ex01/Contact.cpp
+++ cpp-00/ex01/Contact.cpp
@@ -15,89 +15,110 @@
 #include <iostream>
 
 Contact::Contact() {
-    _firstname = "";
-    _lastname = "";
-    _nickname = "";
-    _number = "";
-    _secret = "";
+  _firstname = "";
+  _lastname = "";
+  _nickname = "";
+  _number = "";
+  _secret = "";
 }
 
 Contact::Contact(const std::string &firstname, const std::string &lastname,
                  const std::string &nickname, const std::string &phone_number,
                  const std::string &secret) {
-    _firstname = firstname;
-    _lastname = lastname;
-    _nickname = nickname;
-    _number = phone_number;
-    _secret = secret;
+  _firstname = firstname;
+  _lastname = lastname;
+  _nickname = nickname;
+  _number = phone_number;
+  _secret = secret;
 }
 
 bool Contact::_isValidName(const std::string &name) const {
-    if (name.empty()) {
-        std::cout << RED << "Error: Field cannot be empty.\n" << RESET;
-        return (false);
+  if (name.empty()) {
+    std::cout << RED << "Error: Field cannot be empty.\n" << RESET;
+    return (false);
+  }
+  for (size_t i = 0; i < name.length(); i++) {
+    if (!isalpha(name[i]) && name[i] != ' ' && name[i] != '-') {
+      std::cout << RED
+                << "Error: Names can only contain letters, spaces and "
+                   "hyphens.\n"
+                << RESET;
+      return (false);
     }
-    for (size_t i = 0; i < name.length(); i++) {
-        if (!isalpha(name[i]) && name[i] != ' ' && name[i] != '-') {
-            std::cout << RED
-                      << "Error: Names can only contain letters, spaces and "
-                         "hyphens.\n"
-                      << RESET;
-            return (false);
-        }
-    }
-    return true;
+  }
+  return true;
 }
 
 bool Contact::_isValidPhoneNumber(const std::string &number) const {
-    if (number.empty()) {
-        std::cout << RED << "Error: Field cannot be empty.\n" << RESET;
-        return (false);
-    }
-    for (size_t i = 0; i < number.length(); i++) {
-        if (!isdigit(number[i]) && number[i] != ' ') {
-            std::cout
-                << RED
-                << "Error: Phone number can only contain spaces or digits.\n"
+  if (number.empty()) {
+    std::cout << RED << "Error: Field cannot be empty.\n" << RESET;
+    return (false);
+  }
+  for (size_t i = 0; i < number.length(); i++) {
+    if (!isdigit(number[i])) {
+      std::cout << RED << "Error: Phone number can only contain digits.\n"
                 << RESET;
-            return (false);
-        }
+      return (false);
     }
-    return true;
+  }
+  return true;
+}
+
+std::string Contact::trim_spaces(std::string str) {
+  while (!str.empty() && isspace(str[0])) {
+    str.erase(0, 1);
+  }
+  while (!str.empty() && isspace(str[str.length() - 1])) {
+    str.erase(str.length() - 1, str.length());
+  }
+  size_t i = 0;
+  while (i < str.length()) {
+    if (isspace(str[i]) && i + 1 < str.length() && isspace(str[i + 1])) {
+      str.erase(i, 1);
+    } else {
+      i++;
+    }
+  }
+  return str;
 }
 
 // Setters
 bool Contact::setFirstName(const std::string &firstname) {
-    if (!_isValidName(firstname)) return (false);
-    _firstname = firstname;
-    return (true);
+  std::string trimmed = trim_spaces(firstname);
+  if (!_isValidName(trimmed)) return (false);
+  _firstname = trimmed;
+  return (true);
 }
 
 bool Contact::setLastName(const std::string &lastname) {
-    if (!_isValidName(lastname)) return (false);
-    _lastname = lastname;
-    return (true);
+  std::string trimmed = trim_spaces(lastname);
+  if (!_isValidName(trimmed)) return (false);
+  _lastname = trimmed;
+  return (true);
 }
 
 bool Contact::setNickname(const std::string &nickname) {
-    if (!_isValidName(nickname)) return (false);
-    _nickname = nickname;
-    return (true);
+  std::string trimmed = trim_spaces(nickname);
+  if (!_isValidName(trimmed)) return (false);
+  _nickname = trimmed;
+  return (true);
 }
 
 bool Contact::setPhoneNumber(const std::string &number) {
-    if (!_isValidPhoneNumber(number)) return (false);
-    _number = number;
-    return (true);
+  std::string trimmed = trim_spaces(number);
+  if (!_isValidPhoneNumber(trimmed)) return (false);
+  _number = trimmed;
+  return (true);
 }
 
 bool Contact::setSecret(const std::string &secret) {
-    if (secret.empty()) {
-        std::cout << RED << "Error: Field cannot be empty.\n" << RESET;
-        return (false);
-    }
-    _secret = secret;
-    return (true);
+  std::string trimmed = trim_spaces(secret);
+  if (trimmed.empty()) {
+    std::cout << RED << "Error: Field cannot be empty.\n" << RESET;
+    return (false);
+  }
+  _secret = trimmed;
+  return (true);
 }
 
 // Getters
--- cpp-00/ex01/Contact.hpp
+++ cpp-00/ex01/Contact.hpp
@@ -19,34 +19,35 @@
 #define RESET "\033[0m"
 
 class Contact {
-   private:
-    std::string _firstname;
-    std::string _lastname;
-    std::string _nickname;
-    std::string _number;
-    std::string _secret;
+ private:
+  std::string _firstname;
+  std::string _lastname;
+  std::string _nickname;
+  std::string _number;
+  std::string _secret;
 
-    bool _isValidName(const std::string& name) const;
-    bool _isValidPhoneNumber(const std::string& name) const;
+  bool        _isValidName(const std::string& name) const;
+  bool        _isValidPhoneNumber(const std::string& name) const;
+  std::string trim_spaces(std::string str);
 
-   public:
-    Contact();
-    Contact(const std::string& firstname, const std::string& lastname,
-            const std::string& nickname, const std::string& number,
-            const std::string& secret);
-    // Setter methods
-    bool setFirstName(const std::string& firstname);
-    bool setLastName(const std::string& fullname);
-    bool setNickname(const std::string& nickname);
-    bool setPhoneNumber(const std::string& number);
-    bool setSecret(const std::string& secret);
+ public:
+  Contact();
+  Contact(const std::string& firstname, const std::string& lastname,
+          const std::string& nickname, const std::string& number,
+          const std::string& secret);
+  // Setter methods
+  bool setFirstName(const std::string& firstname);
+  bool setLastName(const std::string& fullname);
+  bool setNickname(const std::string& nickname);
+  bool setPhoneNumber(const std::string& number);
+  bool setSecret(const std::string& secret);
 
-    // Getter methods
-    std::string getFirstName() const;
-    std::string getLastName() const;
-    std::string getNickname() const;
-    std::string getPhoneNumber() const;
-    std::string getSecret() const;
+  // Getter methods
+  std::string getFirstName() const;
+  std::string getLastName() const;
+  std::string getNickname() const;
+  std::string getPhoneNumber() const;
+  std::string getSecret() const;
 };
 
 #endif

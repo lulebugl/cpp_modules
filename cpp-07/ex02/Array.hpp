ef9e345294446e60b407fe8c7b900ddc0a3eefd3
llebugle <llebugle@student.s19.be>
2025-06-07 21:14:06 +0200

    added ex02

---
+++ cpp-07/ex02/Array.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llebugle <llebugle@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/07 19:50:38 by llebugle          #+#    #+#             */
+/*   Updated: 2025/06/07 19:55:24 by llebugle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <cstddef>
+#include <stdexcept>
+
+template <typename T>
+class Array {
+   public:
+    Array() : _size(0), _elements(NULL) {};
+    Array(unsigned int n) : _size(n), _elements(new T[n]()) {};
+    ~Array() {
+        delete[] _elements;
+    };
+
+    Array(const Array& other)
+        : _size(other._size), _elements(new T[other._size]()) {
+        for (unsigned int i = 0; i < _size; i++) {
+            this->_elements[i] = other._elements[i];
+        }
+    };
+
+    Array& operator=(const Array& other) {
+        if (this != &other) {
+            delete[] this->_elements;
+            this->_size = other._size;
+            this->_elements = new T[this->_size]();
+            for (unsigned int i = 0; i < _size; i++) {
+                this->_elements[i] = other._elements[i];
+            }
+        }
+        return *this;
+    }
+
+    T& operator[](size_t index) const {
+        if (index >= _size) {
+            throw std::out_of_range("index out of range");
+        }
+        return _elements[index];
+    }
+
+    unsigned int size() const {
+        return _size;
+    }
+
+   private:
+    unsigned int _size;
+    T*           _elements;
+};
+
+#endif
---
+++ cpp-07/ex02/main.cpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llebugle <llebugle@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/07 20:00:20 by llebugle          #+#    #+#             */
+/*   Updated: 2025/06/07 20:00:22 by llebugle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+#include "Array.hpp"
+
+int main() {
+    std::cout << "=== Basic Array Test ===\n";
+    Array<int> numbers(5);
+
+    for (unsigned int i = 0; i < numbers.size(); i++) {
+        numbers[i] = i * 10;
+    }
+
+    std::cout << "Array contents: ";
+    for (unsigned int i = 0; i < numbers.size(); i++) {
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << "\nArray size: " << numbers.size() << std::endl;
+
+    std::cout << "\n=== Copy Constructor Test ===\n";
+    Array<int> copy(numbers);
+
+    std::cout << "Copy contents: ";
+    for (unsigned int i = 0; i < copy.size(); i++) {
+        std::cout << copy[i] << " ";
+    }
+
+    copy[2] = 999;
+
+    std::cout << "\nOriginal after modifying copy: ";
+    for (unsigned int i = 0; i < numbers.size(); i++) {
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << "\nModified copy: ";
+    for (unsigned int i = 0; i < copy.size(); i++) {
+        std::cout << copy[i] << " ";
+    }
+
+    std::cout << "\n\n=== Assignment Operator Test ===\n";
+    Array<int> other(2);
+    other[0] = 100;
+    other[1] = 200;
+
+    std::cout << "Before assignment: ";
+    for (unsigned int i = 0; i < other.size(); i++) {
+        std::cout << other[i] << " ";
+    }
+    std::cout << " (size: " << other.size() << ")" << std::endl;
+
+
+    std::cout << "After assignment: ";
+    other = numbers;
+    for (unsigned int i = 0; i < other.size(); i++) {
+        std::cout << other[i] << " ";
+    }
+    std::cout << " (size: " << other.size() << ")" << std::endl;
+
+    std::cout << "\n=== Exception Test ===\n";
+    try {
+        std::cout << "Trying invalid index..." << std::endl;
+        std::cout << numbers[99];
+    } catch (const std::exception& e) {
+        std::cout << "Exception : " << e.what() << std::endl;
+    }
+
+    return 0;
+}

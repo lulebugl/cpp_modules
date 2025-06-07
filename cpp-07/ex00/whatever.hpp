4292bd97b502ed26bae87c8379fba435a097bdeb
llebugle <llebugle@student.s19.be>
2025-06-07 15:44:57 +0200

    added ex00

---
+++ cpp-07/ex00/whatever.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llebugle <llebugle@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/07 15:16:19 by llebugle          #+#    #+#             */
+/*   Updated: 2025/06/07 15:16:21 by llebugle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+void swap(T& x, T& y) {
+    T tmp;
+
+    tmp = x;
+    x = y;
+    y = tmp;
+}
+
+template <typename T>
+T min(T x, T y) {
+    return x < y ? x : y;
+}
+
+template <typename T>
+T max(T x, T y) {
+    return x > y ? x : y;
+}
+
+#endif
